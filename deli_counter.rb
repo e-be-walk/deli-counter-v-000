e61d542802f243e88df79db4a74b4a7a57737a2d
Erin Walker <erinb0317@hotmail.com>
2018-01-29 01:12:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli, " ")
+def take_a_number(katz_deli, name)
   name == gets.chomp
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.index} in line."

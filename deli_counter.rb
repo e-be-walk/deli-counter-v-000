8416b8cfe79af833d230b5c8c0a362c29963bc42
Erin Walker <erinb0317@hotmail.com>
2018-01-29 00:59:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,10 @@ def line(katz_deli)
     puts "The line is currently empty."
   elsif katz_deli.length != 0
       puts "The line is currently: #{katz_deli.index}. #{name}"
-    
+
   end
 end
 
 def take_a_number(katz_deli)
-  
+
 end

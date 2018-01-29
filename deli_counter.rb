cb613e4a9716dbb56b2886819770b0100ffc2287
Erin Walker <erinb0317@hotmail.com>
2018-01-29 01:05:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,12 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.length == 0
-    puts "The line is currently empty."
-  elsif katz_deli.length != 0
+  if katz_deli.length != 0
       name == gets.chomp
       katz_deli << name
       puts "The line is currently: #{katz_deli.index}. #{name}"
-
+    elsif katz_deli.length == 0
+        puts "The line is currently empty."
   end
 end
 

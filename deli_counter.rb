c3df94a86928236b410058bf41350ef84cc335df
Erin Walker <erinb0317@hotmail.com>
2018-01-29 00:59:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,12 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
-
-    #array << gets.chomp
-    #katz_deli = array.last
+  elsif katz_deli.length != 0
+      puts "The line is currently: #{katz_deli.index}. #{name}"
+    
   end
+end
 
-
+def take_a_number(katz_deli)
+  
 end

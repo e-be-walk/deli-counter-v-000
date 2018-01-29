810165515daf744f5dcebbb28fa215dae288a973
Erin Walker <erinb0317@hotmail.com>
2018-01-29 01:06:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,6 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.length != 0
-      name == gets.chomp
-      katz_deli << name
       puts "The line is currently: #{katz_deli.index}. #{name}"
     elsif katz_deli.length == 0
         puts "The line is currently empty."
@@ -11,5 +9,6 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli)
-
+  name == gets.chomp
+  katz_deli << name
 end

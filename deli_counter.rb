36cac25ca404426bf44b7652c1e00380fe3cdc25
Erin Walker <erinb0317@hotmail.com>
2018-01-29 01:18:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,16 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.length != 0
-      puts "The line is currently: #{katz_deli.index}. #{name}"
-    elsif katz_deli.length == 0
-        puts "The line is currently empty."
+  if katz_deli.length == 0
+      puts "The line is currently empty."
+  else
+    display_line = katz_deli.collect.with_index(1) do |element, index| "#{index}. #{element}"
   end
+  puts "The line is currently: " + display_line.join(" ")
+end
 end
 
 def take_a_number(katz_deli, name)
-  name == gets.chomp
   katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.index} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end

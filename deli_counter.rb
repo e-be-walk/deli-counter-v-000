5c345036f70dac9b664a002c458a7a8e2341edf5
Erin Walker <erinb0317@hotmail.com>
2018-01-29 01:26:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,24 +1,23 @@
-katz_deli = []
-
-def line(katz_deli)
+def line(deli)
   if katz_deli.length == 0
       puts "The line is currently empty."
   else
-    display_line = katz_deli.collect.with_index(1) do |element, index| "#{index}. #{element}"
+    display_line = deli.collect.with_index(1) do |element, index| "#{index}. #{element}"
   end
   puts "The line is currently: " + display_line.join(" ")
 end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.size} in line."
 end
 
-def now_serving(katz_deli)
-  if katz_deli.length == 0
+def now_serving(deli)
+  if deli.length == 0
       puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli[0]}"
+    puts "Currently serving #{deli.first}"
+    deli.shift
   end
 end
